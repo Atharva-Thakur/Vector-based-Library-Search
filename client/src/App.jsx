6b724fc8fc9c1cff758b0d22f44be06b1c8b087f
Shreedhar Deodatta Urawane <urawane03@gmail.com>
2025-02-03 21:12:50 +0530

    initial commit for client

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl text-yellow-600 font-bold">
+        AI Library Management System
+      </h1>
+    </div>
+  );
+};
+
+export default App;
