b5169a3d4c1dfd86db6cbb961aa6fca00c817584
Atharva Thakur <atharva.r.thakur@gmail.com>
2025-02-04 11:04:49 +0000

    delete duplicate code

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DATA_PATH = "data/books_dataset.csv"
-FAISS_INDEX_PATH = "data/faiss_books.index"
-EMBEDDING_MODEL = "all-MiniLM-L6-v2"  # or "sentence-transformers/all-mpnet-base-v2"
-RERANKER_MODEL = "cross-encoder/ms-marco-MiniLM-L-6-v2"  # Re-ranker model
-EMBEDDING_PATH = "data/embeddings/book_embeddings.npy"
