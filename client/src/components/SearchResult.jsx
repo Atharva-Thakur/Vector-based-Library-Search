458d89f2bdd741a0b68f1a0eba4b4925dce3d903
Atharva Thakur <atharva.r.thakur@gmail.com>
2025-02-09 17:47:07 +0000

    UI bug fix

--- client/src/components/SearchResult.jsx
+++ client/src/components/SearchResult.jsx
@@ -18,9 +18,6 @@ const SearchResult = ({ results }) => {
                                     <h1 className='!font-semibold line-clamp-1'>
                                         {res.title}
                                     </h1>
-                                    <a href={res.link} className='shrink-0'>
-                                        <img src={link} alt="link" />
-                                    </a>
                                 </div>
                                 <p className='italic text-gray-600'>{res.author} | {res.year}</p>
                                 <ul className='flex flex-wrap gap-1 my-1'>
@@ -39,7 +36,7 @@ const SearchResult = ({ results }) => {
                                         })()
                                     }
                                 </ul>
-                                <h1 className='line-clamp-5 mt-2 leading-5'>{res.about}</h1>
+                                <h1 className='line-clamp-5 mt-2 leading-5'>{res.description}</h1>
                             </div>
                         )
 
